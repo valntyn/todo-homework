e9fa8f5831445c1140adf60c529c87766dc0b0e9
valntyn <romanenko.valentyn1@gmail.com>
2023-04-28 16:27:39 +0300

    enum for error was added

---
+++ src/types/ErrorMessage.ts
@@ -0,0 +1,8 @@
+export enum ErrorMessage {
+  NONE = '',
+  TITLE = 'Title can\'t be empty',
+  INCORRECT = 'Only ENG letters and ,.!?',
+  EMPTY = 'Every field should be filled',
+  INCORRECT_DATE = 'Please enter the correct year',
+  DATE_GREATER = 'Creation date can not be more than deadline',
+}
