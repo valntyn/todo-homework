7755b0e113af532301ebc0e248e7d23d90d1a8bc
valntyn <romanenko.valentyn1@gmail.com>
2023-05-03 09:04:03 +0300

    persistor and devtoolsExtention were added

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,29 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import filterReducer from './reducers/filterReducer';
 import todoReducer from './reducers/todoReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   todos: todoReducer,
   filter: filterReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
+  composeWithDevTools(),
 );
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
